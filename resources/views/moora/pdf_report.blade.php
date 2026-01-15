970255f0ae37eb1b77278c7350f238703c2125a7
Anggi Irawan <irawananggi76@gmail.com>
2026-01-15 23:37:36 +0700

    First Commit

--- resources/views/moora/pdf_report.blade.php
+++ resources/views/moora/pdf_report.blade.php
@@ -271,6 +271,7 @@
     </div>
 
     <!-- Final Recommendation -->
+    @php
         $bestAltId = array_key_first($valueMoora);
         $bestAlt = $alternatives->firstWhere('id', $bestAltId);
     @endphp
