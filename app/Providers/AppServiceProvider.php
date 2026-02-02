28fea6ac169736041d9a818a72c0afc10867e5bf
Anggi Irawan <irawananggi76@gmail.com>
2026-02-03 01:27:34 +0700

    First Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,25 @@
 <?php
 
+// app/Providers/AppServiceProvider.php
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        // Override public path untuk DomPDF / production
+        if (App::environment('production')) {
+            $this->app->bind('path.public', function() {
+                return base_path() . '/../public_html/spk.anugrahhadi.com';
+            });
+        }
     }
 }
