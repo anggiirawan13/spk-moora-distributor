93058c928082d930f060ecdd963b1ff9ccabb0a5
Anggi Irawan <irawananggi76@gmail.com>
2026-02-04 13:54:12 +0700

    First Commit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +39,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof TokenMismatchException) {
+            if (auth()->check()) {
+                auth()->logout();
+            }
+
+            return redirect()
+                ->route('login')
+                ->with('error', 'Sesi Anda telah berakhir. Silakan login kembali.');
+        }
+
+        return parent::render($request, $exception);
     }
 }
